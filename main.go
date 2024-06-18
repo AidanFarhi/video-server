c33deb660e9207b3626db87412460b0c55cfe9b2
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-06-17 23:09:00 -0400

    added some basic styling and handlers

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "views/index.html")
+}
+
+func StaticHandler() http.Handler {
+	handler := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+	return handler
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/static/", StaticHandler())
+	mux.HandleFunc("/", IndexHandler)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
 }
