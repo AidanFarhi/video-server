166f505a4a0cd5beab5f85b2d0fadfd2773d14f5
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-06-18 23:02:35 -0400

    added some more junk

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
+
+func init() {
+	fmt.Println("init")
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "views/index.html")
 }
 
 func StaticHandler() http.Handler {
-	handler := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
-	return handler
+	return http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/static/", StaticHandler())
 	mux.HandleFunc("/", IndexHandler)
+
+	mux.Handle("/static/", StaticHandler())
 
 	server := http.Server{
 		Addr:    ":8080",
